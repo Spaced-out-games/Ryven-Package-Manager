ebcaef747711137a81d26afda9fe2926ee2018d6
Spaced-out/usr/local/bin/python3 /Users/dfrost/Desktop/code/BrainBoard/GUI/tkinter_classes.py-games <devin.frost4311@gmail.com>
2022-05-04 11:23:11 -0400

    last

--- tools/code_gen.py
+++ tools/code_gen.py
@@ -1,15 +1,24 @@
-from imp import is_builtin
-from inspect import isbuiltin
-import json, torch,pprint
+from inspect import ismodule
+import torch
 
-def dump(obj, level=0, blacklist = []):
-   for a in dir(obj):
 
-      val = getattr(obj, a)
-      if a not in blacklist:
-        print (level*' ', val)
-      else:
-        print(a)
-        blacklist.append(a)
-        dump(val, level=level+1, blacklist = blacklist)
-dump(torch)
+
+def step(obj, path = []):
+	path = path.copy()
+	attrs = [getattr(obj, attr) for attr in dir(obj)]
+	focus = attrs[0]
+	str(type(focus))
+	if str(type(focus)) not in path:
+		path.append(str(type(obj)))
+		return step(focus, path)
+	return[]
+
+'''
+t = step(torch)
+print(*t)
+
+t = step(*t)
+print(*t)
+'''
+out = step(torch.nn)
+print(out)
